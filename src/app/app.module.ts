9b18405efe7cc79ecee5f7578d80877e05d3ad93
Destors <halen54001@gmail.com>
2020-01-03 23:49:54 +0200

    start2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
+import { CollapseModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,8 +14,7 @@ import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot(),
-    BsDropdownModule.forRoot()
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
