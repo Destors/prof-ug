99ab037a3f8f4029776ecb1829e87acd1fe81520
Destors <halen54001@gmail.com>
2020-01-26 21:32:05 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ServicePageComponent } from './service-page/service-page.component';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
-      {path:'', redirectTo: '/', pathMatch: 'full'},
+      {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: '', component: HomePageComponent},
-      {path: 'service', component: ServicePageComponent}
+      {path: 'service', component: ServicePageComponent},
+      {path: 'contacts', component: ContactsPageComponent}
     ]
   }
 ];
