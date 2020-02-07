10796630cdfc51a165d1066967a11a965232dd89
Destors <halen54001@gmail.com>
2020-02-07 23:46:25 +0200

    dialog test 2

--- src/app/service-page/service-page.component.ts
+++ src/app/service-page/service-page.component.ts
@@ -20,7 +20,9 @@ export class ServicePageComponent {
 }
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'dialog-content-example-dialog',
   templateUrl: 'dialog-content-example-dialog.html',
 })
+// tslint:disable-next-line:component-class-suffix
 export class DialogContentExampleDialog {}
