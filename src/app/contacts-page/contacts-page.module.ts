458a0eb000df8da913d20b9dc5fd2b622ec1d2de
Destors <halen54001@gmail.com>
2020-02-12 20:39:00 +0200

    contacts card

--- src/app/contacts-page/contacts-page.module.ts
+++ src/app/contacts-page/contacts-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ContactsPageComponent } from "./contacts-page.component";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [ContactsPageComponent],
   imports: [
     CommonModule,
+    MatCardModule,
     RouterModule.forChild([
       {
         path: "",
