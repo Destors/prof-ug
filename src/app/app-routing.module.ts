0bd5d1018553c13d58b70ea0d65262badcd1e603
Destors <halen54001@gmail.com>
2020-03-25 00:13:13 +0200

    style_fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules,
-    initialNavigation: 'enabled'
+    initialNavigation: "enabled"
 })
   ],
   exports: [RouterModule]
