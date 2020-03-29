280679193c68ff183f19110026085a031c8dc1f3
Destors <halen54001@gmail.com>
2020-03-29 13:18:30 +0300

    new dynamic Meta

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
 
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"]
 })
+export class HomePageComponent implements OnInit {
+  title = "Профессионал Юг - кондиционеры в Николаеве, продажа, установка, обслуживание";
 
-export class HomePageComponent {}
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {
+        name: "keywords",
+        content:
+          // tslint:disable-next-line:max-line-length
+          "Купить кондиционер Николаев, Установить кондиционер в Николаеве, Монтаж кондиционера Николаев, Монтаж кондиционера в николаеве, Установка кондиционеров, Чистка кондицинера, Профилактика кондиционеро."
+      },
+      {
+        name: "description",
+        content:
+          // tslint:disable-next-line:max-line-length
+          "Профессионал Юг город Николаев, выполняем такие виды работ как, Установить кондиционер, Чистка кондиционера, Монтаж кондиционера, Профилактика кондиционера, Купить кондиционер в Николаеве. Установить кондиционер в Николаеве у нас занимает меньше чем 2 часа времени под ключ. Занимаемся установкой кондиционеров в Николаеве более 15 лет. Купить кондиционер в Николаеве, теперь можно в один звонок. Наши консультанты помогут Вам с выбором. При покупке кондиционера у нас, вы получаете полностью бесплатную доставку в подарок."
+      },
+      { name: "robots", content: "index, follow" }
+    ]);
+  }
+}
--- src/app/service-page/service-page.component.ts
+++ src/app/service-page/service-page.component.ts
@@ -1,18 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material";
+import { Title, Meta } from "@angular/platform-browser";
 
 @Component({
   selector: "app-service-page",
   templateUrl: "./service-page.component.html",
   styleUrls: ["./service-page.component.scss"]
 })
-export class ServicePageComponent {
-  constructor(public dialog: MatDialog, private snackBar: MatSnackBar) {}
+export class ServicePageComponent implements OnInit {
+  title = "Услуги ЧП Профессионал Юг";
+
+  constructor(
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private titleService: Title,
+    private meta: Meta,
+  ) {}
 
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
-      duration: 6000,
+      duration: 6000
     });
   }
 
@@ -25,6 +33,14 @@ export class ServicePageComponent {
   }
   openDialogCleanCond() {
     const dialogRef = this.dialog.open(CondCleanDialog);
+  }
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    // tslint:disable-next-line:max-line-length
+    this.meta.updateTag({name: "keywords", content: "Купить кондиционер, ремонт кондиционера, чистка кондициоенра, профилактика кондиционера."});
+    // tslint:disable-next-line:max-line-length
+    this.meta.updateTag({name: "description", content: "Установка кондиционеров любой сложности, официальная гарантия на выполненые работы."});
   }
 }
 
