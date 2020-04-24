97267bb2e9f2b442b2159b08ad3a628f32bc1460
Destors <halen54001@gmail.com>
2020-04-24 21:44:48 +0300

    scroll_link-fix

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -4,11 +4,15 @@ import { Title, Meta } from "@angular/platform-browser";
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
-  styleUrls: ["./home-page.component.scss"]
+  styleUrls: ["./home-page.component.scss"],
 })
 export class HomePageComponent implements OnInit {
   title =
     "Профессионал Юг - кондиционеры в Николаеве, продажа, установка, обслуживание";
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+  }
 
   constructor(private titleService: Title, private metaService: Meta) {}
 
@@ -19,13 +23,13 @@ export class HomePageComponent implements OnInit {
         name: "keywords",
         content:
           // tslint:disable-next-line:max-line-length
-          "Купить кондиционер Николаев, Установить кондиционер в Николаеве, Монтаж кондиционера Николаев, Монтаж кондиционера в николаеве, Установка кондиционеров, Чистка кондицинера, Профилактика кондиционеро."
+          "Купить кондиционер Николаев, Установить кондиционер в Николаеве, Монтаж кондиционера Николаев, Монтаж кондиционера в николаеве, Установка кондиционеров, Чистка кондицинера, Профилактика кондиционеро.",
       },
       {
         name: "description",
         content:
           // tslint:disable-next-line:max-line-length
-          "Профессионал Юг город Николаев, выполняем такие виды работ как, Установить кондиционер, Чистка кондиционера, Монтаж кондиционера, Профилактика кондиционера, Купить кондиционер в Николаеве. Установить кондиционер в Николаеве у нас занимает меньше чем 2 часа времени под ключ. Занимаемся установкой кондиционеров в Николаеве более 15 лет. Купить кондиционер в Николаеве, теперь можно в один звонок. Наши консультанты помогут Вам с выбором. При покупке кондиционера у нас, вы получаете полностью бесплатную доставку в подарок."
+          "Профессионал Юг город Николаев, выполняем такие виды работ как, Установить кондиционер, Чистка кондиционера, Монтаж кондиционера, Профилактика кондиционера, Купить кондиционер в Николаеве. Установить кондиционер в Николаеве у нас занимает меньше чем 2 часа времени под ключ. Занимаемся установкой кондиционеров в Николаеве более 15 лет. Купить кондиционер в Николаеве, теперь можно в один звонок. Наши консультанты помогут Вам с выбором. При покупке кондиционера у нас, вы получаете полностью бесплатную доставку в подарок.",
       },
       { name: "robots", content: "index, follow" },
       { property: "og:type", content: "website" },
@@ -34,16 +38,16 @@ export class HomePageComponent implements OnInit {
       {
         property: "og:description",
         content:
-          "Установка, продажа и профилактика кондиционеров в городе Николаев"
+          "Установка, продажа и профилактика кондиционеров в городе Николаев",
       },
       { property: "og:url", content: "https://prof-ug.com" },
       { property: "og:locale", content: "ru_RU" },
       {
         property: "og:image",
-        content: "https://prof-ug.web.app/assets/img/og_home.jpg"
+        content: "https://prof-ug.web.app/assets/img/og_home.jpg",
       },
       { property: "og:image:width", content: "968" },
-      { property: "og:image:height", content: "751" }
+      { property: "og:image:height", content: "751" },
     ]);
   }
 }
