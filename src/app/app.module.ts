f20ce0086472fb0238ed3938d08afb18ee781beb
Destors <halen54001@gmail.com>
2020-04-24 21:06:06 +0300

    team_start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { CollapseModule } from "ngx-bootstrap";
 import { MatIconModule } from "@angular/material/icon";
 import { ScrollTopService } from "./services/scrolltop.service";
@@ -18,6 +19,7 @@ import { ScrollTopService } from "./services/scrolltop.service";
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
     CollapseModule.forRoot(),
   ],
   providers: [ScrollTopService],
